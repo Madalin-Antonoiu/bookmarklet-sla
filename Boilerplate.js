501a99df4edf5b27264e589c19cd504942255cf5
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-04-09 07:51:28 +0300

    Add files via upload

---
+++ Boilerplate.js
@@ -0,0 +1,9 @@
+//Boilerplate for the html code
+
+// javascript:(function(){
+//     let script = document.createElement('script');
+//     var url = 'bookmarklet.js';
+//     script.src = url + "?" + new Date().getTime();
+//     document.body.appendChild(script);
+// })();
+
--- bookmarklet.js
+++ bookmarklet.js
@@ -1,9 +1,36 @@
-//Nameless,self-executing function
 (function(){
     console.log('Bookmarklet starting.')
+    
+    var $ = function(id) { return document.getElementById(id)};
+
+    Document.prototype.queryXPath = function (path) {
+
+        var list = new Array()
+      
+        var xpath = document.evaluate(
+          path,
+          document,
+          null,
+          XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+          null
+        )
+      
+        for (var i=0; i<xpath.snapshotLength; i++) {
+      
+          list.push(xpath.snapshotItem(i))
+      
+        }
+      
+        return list
+      
+    }
+
+    //https://servicedesk.csiltd.co.uk/WOListView.do?requestViewChanged=true&viewName=39205_MyView&globalViewName=All_Requests
+    // var win = window.open('https://servicedesk.csiltd.co.uk/WOAdvancedSearch.do');
+
     let paragraphs = document.getElementsByTagName('p');
     for (i=0; i<paragraphs.length; i++){
-        paragraphs[i].innerHTML = "kitten";
+        paragraphs[i].innerHTML = "apples";
     }
 })();
 
